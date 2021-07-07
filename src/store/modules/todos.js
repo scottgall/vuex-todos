ee57d3808aab866b9c3b79a8d88c50103b5b4242
Scott Gall <scottarthurgall@gmail.com>
2021-07-06 23:10:09 -0500

    hook up vuex to todos component

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,10 +1,21 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 const state = {
-
+  todos: [
+    {
+      id: 1,
+      title: 'Todo One'
+    },
+    {
+      id: 2,
+      title: 'Todo Two'
+    }
+  ]
 };
 
-const getters = {};
+const getters = {
+  allTodos: (state) => state.todos
+};
 
 const actions = {};
 
